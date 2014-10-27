94ee329dc1dd3da5ca15283d03c6bf83adf58d52
stockholmfox <stockholmfox@gmail.com>
2014-10-27 15:29:27 +0900

    Backend codes written in Java

---
+++ backend(java)/Rakumono/src/api/RakutenIchiba.java
@@ -0,0 +1,22 @@
+package api;
+
+public class RakutenIchiba {
+	
+	private final static String appID = "";
+	private final static String appRoute = "";
+	
+	/**
+	 * methods to retrieve items using Rakuten API
+	 * 
+	 * @return
+	 */
+	public String ichibaItemSearch(String keyword){
+		
+		
+		return "";
+	}
+	
+	private String urlConstructor(String keyword, String genreID, String itemCode, String shopCode){
+		return "";
+	}
+}
