97552446595dd21250bca44b1b8e0edfe17ef355
asakawa ryu <ryua775@gmail.com>
2014-11-04 12:02:59 +0900

    update subject template

--- server.rb
+++ server.rb
@@ -2,24 +2,29 @@
 require 'sinatra'
 require 'json'
 
+
 get '/' do
   erb :index
 end
 
-get '/suiei.json' do 
-  content_type :json
-  jstr = File.read 'data/suiei.json'
-  json = JSON.parse jstr
+get '/:name' do
+  def read_json subject
+    jstr = File.read "data/#{subject}.json"
+    json = JSON.parse jstr
 
-  data = []
-  json.each do |genre|
-    items = []
-    genre['items'].each do |item|
-      imageUrl = item['mediumImageUrls'][0]['imageUrl']
-      items << item.delete_if {|k,v| ['affiliateRate', 'affiliateUrl', 'asurakuArea', 'asurakuClosingTime', 'asurakuFlag', 'availability', 'catchcopy', 'creditCardFlag', 'imageFlag', 'itemCode', 'shopUrl', 'endTime', 'genreId', 'giftFlag', 'itemCaption', 'pointRateEndTime', 'pointRateStartTime', 'postageFlag', 'shipOverseasArea', 'shipOverseasFlag', 'shopAffiliateUrl', 'shopCode', 'shopName', 'shopOfTheYearFlag','smallImageUrls', 'startTime','taxFlag','mediumImageUrls'].include? k}.merge({"imageUrl" => imageUrl})
+    data = {}
+    json.each do |genre|
+      items = []
+      genre['items'].each do |item|
+        imageUrl = item['mediumImageUrls'][0]['imageUrl']
+        items << item.delete_if {|k,v| ['affiliateRate', 'affiliateUrl', 'asurakuArea', 'asurakuClosingTime', 'asurakuFlag', 'availability', 'catchcopy', 'creditCardFlag', 'imageFlag', 'itemCode', 'shopUrl', 'endTime', 'genreId', 'giftFlag', 'itemCaption', 'pointRateEndTime', 'pointRateStartTime', 'postageFlag', 'shipOverseasArea', 'shipOverseasFlag', 'shopAffiliateUrl', 'shopCode', 'shopName', 'shopOfTheYearFlag','smallImageUrls', 'startTime','taxFlag','mediumImageUrls'].include? k}.merge({"imageUrl" => imageUrl.gsub("128x128","200x200")})
+      end
+      data.merge!({genre['genreName'] => items})
     end
-    data << {genre['genreName'] => items}
+    data
   end
-  data.to_json
-
+  data = read_json params[:name]
+  erb :subject, :locals => {:data => data}
 end
+
+__END__
