3b2e6152e21f23e9dbdbf886732c21a6547c4ced
asakawa ryu <ryua775@gmail.com>
2014-11-07 10:46:48 +0900

    refactor helper read_json

--- app.rb
+++ app.rb
@@ -9,6 +9,26 @@ require_relative 'routes/init'
 
 
 class RakApp < Sinatra::Base
+
+  helpers do 
+    def read_json subject
+      jstr = File.read "#{APPROOT}/data/#{subject}.json"
+      json = JSON.parse jstr
+
+      data = {}
+      json.each do |genre|
+        items = []
+        genre['items'].each do |item|
+          imageUrl = item['mediumImageUrls'][0]['imageUrl']
+          itemName = item['itemName'][0..35] + "..."
+          items << item.delete_if {|k,v| ['affiliateRate', 'affiliateUrl', 'asurakuArea', 'asurakuClosingTime', 'asurakuFlag', 'availability', 'catchcopy', 'creditCardFlag', 'imageFlag', 'itemCode', 'shopUrl', 'endTime', 'genreId', 'giftFlag', 'itemCaption', 'pointRateEndTime', 'pointRateStartTime', 'postageFlag', 'shipOverseasArea', 'shipOverseasFlag', 'shopAffiliateUrl', 'shopCode', 'shopName', 'shopOfTheYearFlag','smallImageUrls', 'startTime','taxFlag','mediumImageUrls'].include? k}.merge({"imageUrl" => imageUrl.gsub("128x128","300x300")}).update({"itemName" => itemName})
+        end
+        data.merge!({genre['genreName'] => items})
+      end
+      data
+    end
+  end
+
   get '/' do
     erb :index
   end
@@ -27,22 +47,6 @@ class RakApp < Sinatra::Base
   end
 
   get '/:name' do
-    def read_json subject
-      jstr = File.read "#{APPROOT}/data/#{subject}.json"
-      json = JSON.parse jstr
-
-      data = {}
-      json.each do |genre|
-        items = []
-        genre['items'].each do |item|
-          imageUrl = item['mediumImageUrls'][0]['imageUrl']
-          itemName = item['itemName'][0..35] + "..."
-          items << item.delete_if {|k,v| ['affiliateRate', 'affiliateUrl', 'asurakuArea', 'asurakuClosingTime', 'asurakuFlag', 'availability', 'catchcopy', 'creditCardFlag', 'imageFlag', 'itemCode', 'shopUrl', 'endTime', 'genreId', 'giftFlag', 'itemCaption', 'pointRateEndTime', 'pointRateStartTime', 'postageFlag', 'shipOverseasArea', 'shipOverseasFlag', 'shopAffiliateUrl', 'shopCode', 'shopName', 'shopOfTheYearFlag','smallImageUrls', 'startTime','taxFlag','mediumImageUrls'].include? k}.merge({"imageUrl" => imageUrl.gsub("128x128","300x300")}).update({"itemName" => itemName})
-        end
-        data.merge!({genre['genreName'] => items})
-      end
-      data
-    end
     if (File.exists? "#{APPROOT}/data/#{params[:name]}.json")
       data = read_json params[:name] 
     else
