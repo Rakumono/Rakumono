88b3ee79c9018c867aa87d95acddbd95043856e6
asakawa ryu <ryua775@gmail.com>
2014-11-19 17:54:15 +0900

    Update json api

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'haml'
 gem 'sinatra'
+gem 'thin'
--- routes/index.rb
+++ routes/index.rb
@@ -1,10 +1,27 @@
-
+require 'net/http'
+require 'json'
 
 class RakApp < Sinatra::Base
+  include Net
+  API_HOST = "http://rakumono.cloudapp.net:8080/wordfind/index.jsp?"
   get '/' do
     haml :index
   end
+  
   get '/search' do
     haml :search
   end
+
+  post '/' do
+    content_type :json
+    keywords = params[:keyword].split ","
+    string = HTTP.get URI("#{API_HOST}api_num=1&keywords=#{keywords.join("%3E")}")
+  end
+
+  get '/item' do
+    content_type :json
+    item_num = 3 unless params['item_num']
+    string = HTTP.get URI("#{API_HOST}api_num=2&shopname=#{params[:shopname]}&word=#{params[:keyword]}&item_num=#{item_num}")
+  end
+
 end
